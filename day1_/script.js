546f7b3a174cfccdbc31c14acbffcfc1d31ead9e
ashutoshvats03 <142529686+ashutoshvats03@users.noreply.github.com>
2025-02-08 22:52:01 +0530

    unbug commit

---
+++ day1_/script.js
@@ -0,0 +1,11 @@
+let box = document.getElementById("box")
+
+// box.addEventListener("click", (e)=>{
+//         e.stopPropagation();
+//     alert("bhag bhago")
+// })
+
+box.onkeydown=(e)=>{
+    alert("kkk");
+    console.log(e);
+};
